3becba716d7b5c5af27da908a59ce36f0f88c6a5
Denys Gusti <90290236+DenysGusti@users.noreply.github.com>
2022-09-08 05:07:15 +0300

    updated

--- cpp_merger.py
+++ cpp_merger.py
@@ -5,11 +5,11 @@ from subprocess import run
 class Merger:
     def __init__(self, *, project_name: str, merge_exe: Path, inpath: Path, outpath: Path, work_dir: Path,
                  tasks: list[str]):
-        self.project_name = project_name
-        self.merge_exe = merge_exe
-        self.inpath = inpath
-        self.outpath = outpath
-        self.work_dir = work_dir
+        self.project_name: str = project_name
+        self.merge_exe: Path = merge_exe
+        self.inpath: Path = inpath
+        self.outpath: Path = outpath
+        self.work_dir: Path = work_dir
         self.tasks: list[str] = tasks
 
     def merge(self) -> 'Merger':
